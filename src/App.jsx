803f8ea5f2985af7214143d3d44a3cb5883e45ad
Nazmul Haque <bpinazmul18@gmail.com>
2022-10-28 19:37:01 +0600

    Replace app component with class based component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const App = () => {
-  return <div>Hello world</div>
+class App extends React.Component {
+  render() {
+    return <div>Hello world</div>
+  }
 }
-
 export default App
