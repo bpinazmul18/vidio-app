100e78ea27ebc59ca8f5cc23e210e23f4cc43af3
Nazmul haque <bpinazmul18@gmail.com>
2022-10-30 12:27:24 +0600

    Update state using fetch data.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import SearchBar from './components/SearchBar'
 import { getVideos } from './services/youtube'
 
 class App extends React.Component {
+  state = { videos: [] }
+
   onTermSubmit = async (term) => {
     const response = await getVideos(term)
     console.log(response)
+    this.setState({ videos: response.data.items })
   }
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have got{' '}
+        {this.state.videos.length} videos.
       </div>
     )
   }
